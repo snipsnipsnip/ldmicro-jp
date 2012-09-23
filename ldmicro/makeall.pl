ab608d2ee0ec677932b00f7fa78349519603e3c7
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2012-09-23 14:06:41 +0900

    makeall.pl: replace language list with glob

--- ldmicro/makeall.pl
+++ ldmicro/makeall.pl
@@ -5,10 +5,12 @@ sub SYS { system($_[0]); }
 SYS("rm -rf build");
 SYS("mkdir build");
 
-for $f qw(DE ES FR IT PT TR) {
+for my $langfile (<lang-*.txt>) {
+    $langfile =~ /-([^.]+)/;
+    my $f = uc($1);
+    my $fl = lc($1);
     SYS("nmake clean");
     SYS("nmake D=LDLANG_$f");
-    $fl = lc($f);
     SYS("copy ldmicro.exe build\\ldmicro-$fl.exe");
 }
 
